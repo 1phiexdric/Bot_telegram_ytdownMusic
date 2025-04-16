41012ca00244603128bc6e1c2eea3962a7fcf11a
Friedrich Ruiz <ruizlopezfriedrichjavier@gmail.com>
2025-04-16 14:50:43 -0400

    Add files via upload

---
+++ src/app.py
@@ -0,0 +1,44 @@
+import telebot
+import os
+from music import descargar_musica
+import time
+from dotenv import load_dotenv
+load_dotenv()
+
+bot = telebot.TeleBot(os.getenv("BOT_TOKEN"), parse_mode=None)
+telebot.apihelper.CONNECT_TIMEOUT = 60  # Aumenta el tiempo de espera de conexión
+telebot.apihelper.READ_TIMEOUT = 60    # Aumenta el tiempo de espera de lectura
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+    ¡Hola! 👋 Soy YtdownMusic, tu bot genial para descargar tu música de YouTube. 
+Escribe /help!
+    """)
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, """\
+    Envia el link de tu cancion para la descargar
+    """)
+
+@bot.message_handler(content_types=['text'])
+def download(message):
+    try:
+        if message.text.startswith("https://youtu"):
+            msg = bot.reply_to(message, "Descargando..." \
+            "\nEsto puede tardar un poco, por favor espera.")
+            
+            salida = descargar_musica(message.text)
+            with open(salida, 'rb') as cancion:
+                bot.send_chat_action(message.chat.id, 'upload_document')
+                bot.send_document(message.chat.id, cancion)
+            
+            bot.delete_message(message.chat.id, msg.message_id)
+            os.remove(salida)
+        else:
+            bot.reply_to(message, "La url no es valida, asegurate de que sea un link de youtube o este bien escrito.")
+    except Exception as e:
+        bot.reply_to(message, "Ocurrio un error: " + str(e))
+
+
+bot.infinity_polling() 
---
+++ src/music.py
@@ -0,0 +1,46 @@
+import os
+
+import yt_dlp
+
+ruta_actual = os.path.dirname(os.path.abspath(__file__))
+ruta_ffmpeg = os.path.join(ruta_actual, "ffmpeg", "bin")
+
+def descargar_musica(url, carpeta="Descargas"):
+
+    """
+    Descarga la música de un video de YouTube en formato MP3.
+
+    Args:
+        url (str): La URL del video de YouTube.
+        carpeta (str): La carpeta donde se guardará la música descargada. Por defecto es "musica".
+
+    Returns:
+        None
+    """
+
+    try:
+        os.makedirs(carpeta, exist_ok=True)
+        salida = os.path.join(carpeta, '%(title)s.%(ext)s')
+        opciones = {
+            'format': 'bestaudio/best',
+            'outtmpl': f'{carpeta}/%(title)s.%(ext)s', 
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '160',
+        }],
+            'ffmpeg_location': ruta_ffmpeg,
+        }
+        with yt_dlp.YoutubeDL(opciones) as ydl:
+            #ydl.download([url])
+            info = ydl.extract_info(url, download=True)
+            salida = ydl.prepare_filename(info)  # Obtener la ruta del archivo generado
+            salida = os.path.splitext(salida)[0] + ".mp3"  # Cambiar extensión a .mp3
+            return salida
+    except yt_dlp.utils.DownloadError as e:
+        print(f"ocurrio un error: {e}")
+    
+
+if __name__ == "__main__":
+    music_url = input("Ingresa la url del video: ")
+    descargar_musica(music_url)
